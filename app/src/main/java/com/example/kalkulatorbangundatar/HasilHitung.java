549024f3729ea93a62ce9fe58ee9d9dc92617838
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2021-10-20 23:27:10 +0700

    menambahkan activity kedua untuk hasil hitung

---
+++ app/src/main/java/com/example/kalkulatorbangundatar/HasilHitung.java
@@ -0,0 +1,14 @@
+package com.example.kalkulatorbangundatar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HasilHitung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_hitung);
+    }
+}
